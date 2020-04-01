d5978fa8980d739cfef2858a5d4d971611eeabbb
Shirlei Machado <schiufreitas@gmail.com>
2020-04-01 17:38:28 -0300

    incluindo respostas

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -12,18 +12,18 @@
  //= 4, imprime - [12]
 
  //DESAFIO 1////////////////
-/*ele acrescentou 1 zero para cada linha a partir da 2ª linha, só não entendi esse 3 no final
+/*ele acrescentou 1 zero para cada linha a partir da 2ª linha.
  0
 00
 000
 0000
-3 
 */
 
-//EXERCÍCIO 3/////////////////////////////////////////
-/*const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
 
-//a.
+//EXERCÍCIO 3/////////////////////////////////////////
+//const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+//////////////a.
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 let maior=array[0], menor=array[0];
 
@@ -36,9 +36,9 @@ for(let numero of array){
     }
 }
 console.log("O maior número é: ", maior," o menor é: ", menor);
-*/
 
-//b.
+
+//////////////b.
 
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 let Narray = []
@@ -47,5 +47,24 @@ let Narray = []
     }
     console.log(Narray);
 
- //c. Escreva um programa que devolva um novo array contendo, somente, os números pares do array original.
-    
+ /////////////c.
+ const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+ Narray = [];
+
+ for(let numero of array){
+     if(numero%2 === 0){
+        Narray.push(numero);
+     }
+ }
+ console.log(Narray);
+
+
+ /////////////d. 
+ /
+ const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+ Narray = [];
+
+ for(let i=0; i<array.length; i++){
+     Narray.push("O elemento do index "+ i+" é "+ array[i]);
+ }
+ console.log(Narray);
