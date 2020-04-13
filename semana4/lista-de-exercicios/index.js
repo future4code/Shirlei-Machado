9f2330d3efe1d7f9cd77d5146b811b4561afeb5d
Shirlei Machado <schiufreitas@gmail.com>
2020-04-13 07:57:56 -0300

    exercicio1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,27 @@
+// Exercício 1/////////////////////////////
+// booleano1 = true
+// booleano2 = false
+// booleano3 = !booleano2 = true
+// booleano 4 = !booleano3 = false
+// a. true && false && false 
+// false
+// b. (true && false) || !true
+// false || false
+// false
+// c.(false || true) && (false || true)
+// true && true
+// true
+// d.!(false && true) || !(true && true)
+// !false || !true
+// true || false
+// true
+// e.!(true) && !(true) || (!false && true && true)
+// false && false || (true && true && true)
+// false || true
+// true
+// Respostas:
+// a - false
+// b - false
+// c - true
+// d - true
+// e - true
