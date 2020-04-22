87e1393221485d3ecc0ffb62595d8490a4c244f3
Shirlei Machado <schiufreitas@gmail.com>
2020-04-22 17:05:30 -0300

    listas

---
+++ semana6/futureform/futureform/build/precache-manifest.21c9d1fe1848f57f9cbe50d5b5cd313c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a27bf2a54170f7872040aff352f90aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "256ff8cbb1716a5aeb7d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9b79ab64171a106bb64",
+    "url": "/static/js/2.6bb82757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bb82757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256ff8cbb1716a5aeb7d",
+    "url": "/static/js/main.d87a3aaa.chunk.js"
+  },
+  {
+    "revision": "070cdac1555798e52e72",
+    "url": "/static/js/runtime-main.834040a8.js"
+  }
+]);
