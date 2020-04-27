8991a01286526fc2ec63a486aad06fecfdead8c6
Shirlei Machado <schiufreitas@gmail.com>
2020-04-27 16:41:37 -0300

    alteração

---
+++ semana6/aula22/boiler-plate/build/precache-manifest.e443c2f036c4fb5b3dc56148f589099e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa1085819a4e3f5cc9c2160e2be70ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a71a1595d257b6f0ad1",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "cf222051f0498bfb0faf",
+    "url": "/static/js/2.38e5fef9.chunk.js"
+  },
+  {
+    "revision": "8a71a1595d257b6f0ad1",
+    "url": "/static/js/main.48ce1916.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
