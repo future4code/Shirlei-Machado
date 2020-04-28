9383eaee4f4b61bb2c01b4b8fbf5a09fc91b8001
Shirlei Machado <schiufreitas@gmail.com>
2020-04-28 17:03:38 -0300

    Labenusers

---
+++ semana7/aula25/labenusers/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Cadastro from './components/Cadastro';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    inputNome:'',
+    inputEmail:'',
+    cadastros:[],
+  };
+
+  
+}
+export default App;
