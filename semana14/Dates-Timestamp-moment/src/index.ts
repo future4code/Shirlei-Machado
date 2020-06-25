8623c15dce3f48cac1cec21bbf126d68216155f4
Shirlei-Machado <schiufreitas@gmil.com>
2020-06-24 23:58:31 -0300

    incluindo o exercicio2 incompleto

--- semana14/Dates-Timestamp-moment/src/index.ts
+++ semana14/Dates-Timestamp-moment/src/index.ts
@@ -23,3 +23,11 @@ const allEvents: event[] = [
 	 	finishAt: moment("26/06/2020 18:00", "DD/MM/YYYY HH:mm")
 	}
 ]
+
+//2
+let date: moment.Moment = moment("25/06/2020 15:00", "DD/MM/YYYY HH:mm");
+console.log("Nome:", name)
+console.log("Descrição:", description)
+console.log("Início:", date.format("dddd, DD [de] MM [de] YYYY, HH:mm[h]"))
+console.log("Início:", date.format("DD/MM/YYYY, HH:mm[h]"))
+
