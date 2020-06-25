61d67a62954607238c3ee6507b78ad0eb88a03f0
Shirlei-Machado <schiufreitas@gmil.com>
2020-06-24 22:26:06 -0300

    Exercicio1

---
+++ semana14/aula-node-package/build/exercicio1.js
@@ -0,0 +1,6 @@
+"use strict";
+const nome = process.argv[2];
+const age = Number(process.argv[3]);
+console.log('Olá,', nome, '! Você tem', age, 'anos');
+console.log('Olá,', nome, '! Você tem', age, 'anos. Em sete anos você terá', age + 7);
+//# sourceMappingURL=exercicio1.js.map
---
+++ semana14/aula-node-package/src/exercicio1.ts
@@ -0,0 +1,8 @@
+//a- 
+//b- 
+const nome: string = process.argv[2];
+const age: number = Number (process.argv[3]);
+console.log('Olá,', nome,'! Você tem', age, 'anos.')
+
+//c-
+console.log('Olá,', nome,'! Você tem', age, 'anos. Em sete anos você terá', age+7)
