88a05563bf3fbabbf3ce1de1f2759b4250656d09
Shirlei-Machado <schiufreitas@gmil.com>
2020-07-14 01:00:48 -0300

    Exercicios_aula55

---
+++ semana17/aula55/Aula_55.md
@@ -0,0 +1,11 @@
+**Exercicio 1**
+
+*a) Sim , string é bem melhor para o usuário lembrar.
+
+**Exercicio 2**
+
+*a) função de autenticação, precisamos da conexão com o banco e precisamos criar um usuário com id,senha e email.
+
+**Exercicio 3**
+
+ *a) string serve para garantir que o valor não vai ficar undefinied.
---
+++ semana17/aula55/src/services/services.ts
@@ -0,0 +1,30 @@
+import { v4 } from "uuid";
+import * as jwt from "jsonwebtoken";
+
+
+export class IdGenerator {
+  public generate(): string {
+    return v4();
+  }
+
+}
+
+export class Authenticator {
+  private static EXPIRES_IN = "1min";
+  public generateToken(input: AuthenticationData): string {
+    const token = jwt.sign(
+      {
+        id: input.id,
+      },
+      process.env.JWT_KEY as string,
+      {
+        expiresIn: Authenticator.EXPIRES_IN,
+      }
+    );
+    return token;
+  }
+}
+
+interface AuthenticationData {
+  id: string;
+}
